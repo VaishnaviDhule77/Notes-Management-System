1c8236f972ba80cb43f7f57927bac1285a900e1a
VaishnaviDhule77 <vdhule730@gmail.com>
2026-05-05 17:02:26 +0530

    code updated

--- library/index.php
+++ library/index.php
@@ -1,27 +1,8 @@
 <?php
 session_start();
 include('includes/config.php');
-error_reporting(E_ALL); // Use 0 for live, E_ALL for debugging
-ini_set('display_errors', 1);
 
-// Clear session if they hit this page to ensure a fresh login
-if(isset($_SESSION['login']) && $_SESSION['login'] != ''){
-    $_SESSION['login'] = '';
-}
-// ... rest of your login logic ...
-?>
-<?php
-
-if(!isset($_SESSION['login'])) {
-    header("Location: adminlogin.php");
-    exit();
-}
-?>
-<?php
-session_start();
-include('includes/config.php');
-
-// 1. CLEAR ERRORS FOR THE LIVE SITE (Recommended for your placement)
+// 1. CLEAR ERRORS FOR THE LIVE SITE 
 error_reporting(0); 
 ini_set('display_errors', 0);
 
@@ -33,7 +14,6 @@ if(isset($_SESSION['login']) && $_SESSION['login'] != '') {
 // 3. LOGIN PROCESSING
 if(isset($_POST['login'])) {
     $email = $_POST['emailid'];
-    // Using md5 because your project currently uses it for passwords
     $password = md5($_POST['password']); 
     
     $sql = "SELECT EmailId, Password, StudentId, Status FROM tblstudents WHERE EmailId=:email and Password=:password";
@@ -63,107 +43,71 @@ if(isset($_POST['login'])) {
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <title>Online Library Management System | </title>
-    <!-- BOOTSTRAP CORE STYLE  -->
+    <title>Online Library Management System</title>
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
-    <!-- FONT AWESOME STYLE  -->
     <link href="assets/css/font-awesome.css" rel="stylesheet" />
-    <!-- CUSTOM STYLE  -->
     <link href="assets/css/style.css" rel="stylesheet" />
-    <!-- GOOGLE FONT -->
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
-
 </head>
 <body>
-    <!------MENU SECTION START-->
 <?php include('includes/header.php');?>
-<!-- MENU SECTION END-->
+
 <div class="content-wrapper">
 <div class="container">
-<!--Slider---->
-     <div class="row">
-              <div class="col-md-10 col-sm-8 col-xs-12 col-md-offset-1">
-                    <div id="carousel-example" class="carousel slide slide-bdr" data-ride="carousel" >
-                    <div class="carousel-inner">
-                        <div class="item active">
-                            <img src="assets/img/img1.jpeg" alt=""  />
-                        </div>
-                        <div class="item">
-                            <img src="assets/img/img2.jpeg" alt="" />
-                        </div>
-                        <div class="item">
-                            <img src="assets/img/img3.jpeg" alt=""  /> 
-                        </div>
-                    </div>
-                    <!--INDICATORS-->
-                     <ol class="carousel-indicators">
-                        <li data-target="#carousel-example" data-slide-to="0" class="active"></li>
-                        <li data-target="#carousel-example" data-slide-to="1"></li>
-                        <li data-target="#carousel-example" data-slide-to="2"></li>
-                    </ol>
-                    <!--PREVIUS-NEXT BUTTONS-->
-                     <a class="left carousel-control" href="#carousel-example" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left"></span>
-  </a>
-  <a class="right carousel-control" href="#carousel-example" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right"></span>
-  </a>
+    <!--Slider---->
+    <div class="row">
+        <div class="col-md-10 col-sm-8 col-xs-12 col-md-offset-1">
+            <div id="carousel-example" class="carousel slide slide-bdr" data-ride="carousel" >
+                <div class="carousel-inner">
+                    <div class="item active"><img src="assets/img/img1.jpeg" alt="" /></div>
+                    <div class="item"><img src="assets/img/img2.jpeg" alt="" /></div>
+                    <div class="item"><img src="assets/img/img3.jpeg" alt="" /></div>
                 </div>
-              </div>
-             </div>
-<hr />
-
-
-
-<div class="row pad-botm">
-<div class="col-md-12">
-<h4 class="header-line">USER LOGIN FORM</h4>
-</div>
-</div>
- <a name="ulogin"></a>            
-<!--LOGIN PANEL START-->           
-<div class="row">
-<div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3" >
-<div class="panel panel-info">
-<div class="panel-heading">
- LOGIN FORM
-</div>
-<div class="panel-body">
-<form role="form" method="post">
-
-<div class="form-group">
-<label>Enter Email id</label>
-<input class="form-control" type="text" name="emailid" required autocomplete="off" />
-</div>
-<div class="form-group">
-<label>Password</label>
-<input class="form-control" type="password" name="password" required autocomplete="off"  />
-<p class="help-block"><a href="user-forgot-password.php">Forgot Password</a></p>
-</div>
-
-
-
- <button type="submit" name="login" class="btn btn-info">LOGIN </button> | <a href="signup.php">Not Register Yet</a>
-</form>
- </div>
-</div>
-</div>
-</div>  
-<!---LOGIN PABNEL END-->            
-             
- 
+                <ol class="carousel-indicators">
+                    <li data-target="#carousel-example" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-example" data-slide-to="1"></li>
+                    <li data-target="#carousel-example" data-slide-to="2"></li>
+                </ol>
+                <a class="left carousel-control" href="#carousel-example" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+                <a class="right carousel-control" href="#carousel-example" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
+        </div>
     </div>
-    </div>
-     <!-- CONTENT-WRAPPER SECTION END-->
- <?php include('includes/footer.php');?>
-      <!-- FOOTER SECTION END-->
-    <script src="assets/js/jquery-1.10.2.js"></script>
-    <!-- BOOTSTRAP SCRIPTS  -->
-    <script src="assets/js/bootstrap.js"></script>
-      <!-- CUSTOM SCRIPTS  -->
-    <script src="assets/js/custom.js"></script>
+    <hr />
 
+    <div class="row pad-botm">
+        <div class="col-md-12">
+            <h4 class="header-line">USER LOGIN FORM</h4>
+        </div>
+    </div>
+    
+    <div class="row">
+        <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3" >
+            <div class="panel panel-info">
+                <div class="panel-heading">LOGIN FORM</div>
+                <div class="panel-body">
+                    <form role="form" method="post">
+                        <div class="form-group">
+                            <label>Enter Email id</label>
+                            <input class="form-control" type="text" name="emailid" required autocomplete="off" />
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input class="form-control" type="password" name="password" required autocomplete="off" />
+                            <p class="help-block"><a href="user-forgot-password.php">Forgot Password</a></p>
+                        </div>
+                        <button type="submit" name="login" class="btn btn-info">LOGIN </button> | <a href="signup.php">Not Register Yet</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>  
+</div>
+</div>
+
+<?php include('includes/footer.php');?>
+<script src="assets/js/jquery-1.10.2.js"></script>
+<script src="assets/js/bootstrap.js"></script>
+<script src="assets/js/custom.js"></script>
 </body>
 </html>

939b576ac387f75c8e54ea3fa12bf2d75555b24e
VaishnaviDhule77 <vdhule730@gmail.com>
2026-05-05 15:13:01 +0530

    code updated

--- library/admin/includes/config.php
+++ library/admin/includes/config.php
@@ -1,16 +1,15 @@
-<?php 
-// DB credentials.
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','notes');
-// Establish database connection.
-try
-{
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-}
-catch (PDOException $e)
-{
-exit("Error: " . $e->getMessage());
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+try {
+    $dbh = new PDO(
+        "mysql:host=tramway.proxy.rlwy.net;port=57323;dbname=railway",
+        "root",
+        "qrrAyQvwCpvyUsSHZGqufKaWXfSIUINs"
+    );
+    //echo "DB CONNECTED";
+} catch (PDOException $e) {
+    die("DB ERROR: " . $e->getMessage());
 }
 ?>

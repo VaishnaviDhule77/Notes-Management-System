81d52e126da9ba106c74815cb9610420fd413b3c
VaishnaviDhule77 <vdhule730@gmail.com>
2026-08-31 12:56:58 +0530

    Updated files

--- library/adminlogin.php
+++ library/adminlogin.php
@@ -1,34 +1,39 @@
 <?php
-error_reporting(0);
-ini_set('display_errors', 0);
-?>
-<?php
 session_start();
-
 $_SESSION['alogin'] = "";
-error_reporting(0);
+
+// Display errors for debugging production/host issues
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 include('includes/config.php');
-if($_SESSION['alogin']!=''){
-$_SESSION['alogin']='';
-}
-if(isset($_POST['login']))
-{
-$username=$_POST['username'];
-$password=md5($_POST['password']);
-$sql ="SELECT UserName,Password FROM admin WHERE UserName=:username and Password=:password";
-$query= $dbh -> prepare($sql);
-$query-> bindParam(':username', $username, PDO::PARAM_STR);
-$query-> bindParam(':password', $password, PDO::PARAM_STR);
-$query-> execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-if($query->rowCount() > 0)
-{
-$_SESSION['alogin']=$_POST['username'];
-header("Location: admin/dashboard.php");
-exit();
-} else{
-echo "<script>alert('Invalid Details');</script>";
-}
+
+if(isset($_POST['login'])) {
+    try {
+        $username = trim($_POST['username']);
+        $password = md5($_POST['password']);
+        
+        // Execute SQL check against the database
+        $sql = "SELECT UserName, Password FROM admin WHERE UserName=:username AND Password=:password";
+        $query = $dbh->prepare($sql);
+        $query->bindParam(':username', $username, PDO::PARAM_STR);
+        $query->bindParam(':password', $password, PDO::PARAM_STR);
+        $query->execute();
+        $results = $query->fetchAll(PDO::FETCH_OBJ);
+
+        if($query->rowCount() > 0) {
+            $_SESSION['alogin'] = $username;
+            // Correct relative path for dashboard inside admin directory
+            header("Location: dashboard.php");
+            exit();
+        } else {
+            echo "<script>alert('Invalid Details');</script>";
+        }
+    } catch (PDOException $e) {
+        // Stop execution and print exact SQL failure instead of showing blank HTTP 500
+        die("LOGIN DB ERROR: " . $e->getMessage());
+    }
 }
 ?>
 <!DOCTYPE html>
@@ -38,7 +43,7 @@ echo "<script>alert('Invalid Details');</script>";
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Online notes Management System</title>
+    <title>Online Notes Management System - Admin Login</title>
     <!-- BOOTSTRAP CORE STYLE  -->
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
     <!-- FONT AWESOME STYLE  -->
@@ -46,59 +51,49 @@ echo "<script>alert('Invalid Details');</script>";
     <!-- CUSTOM STYLE  -->
     <link href="assets/css/style.css" rel="stylesheet" />
     <!-- GOOGLE FONT -->
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
-
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
 </head>
 <body>
-    <!------MENU SECTION START-->
-<?php include('includes/header.php');?>
-<!-- MENU SECTION END-->
-<div class="content-wrapper">
-<div class="container">
-<div class="row pad-botm">
-<div class="col-md-12">
-<h4 class="header-line">ADMIN LOGIN FORM</h4>
-</div>
-</div>
-             
-<!--LOGIN PANEL START-->           
-<div class="row">
-<div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3" >
-<div class="panel panel-info">
-<div class="panel-heading">
- LOGIN FORM
-</div>
-<div class="panel-body">
-<form role="form" method="post">
+    <?php include('includes/header.php');?>
 
-<div class="form-group">
-<label>Enter Username</label>
-<input class="form-control" type="text" name="username" autocomplete="off" required />
-</div>
-<div class="form-group">
-<label>Password</label>
-<input class="form-control" type="password" name="password" autocomplete="off" required />
-</div>
+    <div class="content-wrapper">
+        <div class="container">
+            <div class="row pad-botm">
+                <div class="col-md-12">
+                    <h4 class="header-line">ADMIN LOGIN FORM</h4>
+                </div>
+            </div>
+                     
+            <div class="row">
+                <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                    <div class="panel panel-info">
+                        <div class="panel-heading">
+                            LOGIN FORM
+                        </div>
+                        <div class="panel-body">
+                            <form role="form" method="post">
+                                <div class="form-group">
+                                    <label>Enter Username</label>
+                                    <input class="form-control" type="text" name="username" autocomplete="off" required />
+                                </div>
+                                <div class="form-group">
+                                    <label>Password</label>
+                                    <input class="form-control" type="password" name="password" autocomplete="off" required />
+                                </div>
 
- <button type="submit" name="login" class="btn btn-info">LOGIN </button>
-</form>
- </div>
-</div>
-</div>
-</div>  
-<!---LOGIN PABNEL END-->            
-             
- 
+                                <button type="submit" name="login" class="btn btn-info">LOGIN </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>  
+        </div>
     </div>
-    </div>
-     <!-- CONTENT-WRAPPER SECTION END-->
- <?php include('includes/footer.php');?>
-      <!-- FOOTER SECTION END-->
+
+    <?php include('includes/footer.php');?>
+
     <script src="assets/js/jquery-1.10.2.js"></script>
-    <!-- BOOTSTRAP SCRIPTS  -->
     <script src="assets/js/bootstrap.js"></script>
-      <!-- CUSTOM SCRIPTS  -->
     <script src="assets/js/custom.js"></script>
-</script>
 </body>
 </html>
